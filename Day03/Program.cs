78a990ca90dbeca4f6f43264847ce0d54fd95425
Lee Chengkai <leechengkai@gmail.com>
2017-12-03 17:15:24 +0800

    Finish 3-2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -13,7 +13,16 @@ namespace Day03
             // PartOne(1024);
 
             // Usage: dotnet run -- <your input here>
-            PartOne(Int32.Parse(args[0]));
+            // PartOne(Int32.Parse(args[0]));
+
+            //var j = 1;
+            //for (int i = 0; i < 23; i++)
+            //{
+            //   j = PartTwo(j);
+            //}
+
+            PartTwo(Int32.Parse(args[0]));
+
             Console.ReadLine();
         }
 
@@ -22,6 +31,7 @@ namespace Day03
             int ring = 0;
             int current = 1;
 
+            // Find the largest bottom right corner
             while (Math.Pow(2 * (ring + 1) + 1, 2) < number)
             {
                 ring++;
@@ -77,6 +87,88 @@ namespace Day03
             Console.WriteLine("distance: " + (Math.Abs(xOffset) + Math.Abs(yOffset)));
         }
 
+        static int PartTwo(int number)
+        {
+            int ring = 0;
+            int value = 0;
+
+            int xOffset = ring;
+            int yOffset = -ring;
+
+            List<GridCell> Grid = new List<GridCell>(number) {
+                new GridCell() { Value= 1, XOffset = 0, YOffset = 0 }
+            };
+
+            Direction direction = Direction.Right;
+
+            while (value <= number)
+            {
+                if (direction == Direction.Right)
+                {
+                    xOffset++;
+                    if (xOffset > ring)
+                    {
+                        ring++;
+                        direction = Direction.Up;
+                    }
+                }
+                else if (direction == Direction.Up)
+                {
+                    yOffset++;
+                    if (yOffset >= ring)
+                    {
+                        direction = Direction.Left;
+                    }
+                }
+                else if (direction == Direction.Left)
+                {
+                    xOffset--;
+                    if (xOffset <= -ring)
+                    {
+                        direction = Direction.Down;
+                    }
+                }
+                else if (direction == Direction.Down)
+                {
+                    yOffset--;
+                    if (yOffset <= -ring)
+                    {
+                        direction = Direction.Right;
+                    }
+                }
+
+                GridCell cell = new GridCell() {
+                    XOffset = xOffset,
+                    YOffset = yOffset
+                };
+
+                value = 0;
+
+                // Add the 8 adjacent values
+                value += Grid.Find(c => c.XOffset == xOffset + 1 && c.YOffset == yOffset + 1)?.Value ?? 0;
+                value += Grid.Find(c => c.XOffset == xOffset + 1 && c.YOffset == yOffset + 0)?.Value ?? 0;
+                value += Grid.Find(c => c.XOffset == xOffset + 1 && c.YOffset == yOffset - 1)?.Value ?? 0;
+                value += Grid.Find(c => c.XOffset == xOffset + 0 && c.YOffset == yOffset + 1)?.Value ?? 0;
+                value += Grid.Find(c => c.XOffset == xOffset + 0 && c.YOffset == yOffset - 1)?.Value ?? 0;
+                value += Grid.Find(c => c.XOffset == xOffset - 1 && c.YOffset == yOffset + 1)?.Value ?? 0;
+                value += Grid.Find(c => c.XOffset == xOffset - 1 && c.YOffset == yOffset + 0)?.Value ?? 0;
+                value += Grid.Find(c => c.XOffset == xOffset - 1 && c.YOffset == yOffset - 1)?.Value ?? 0;
+
+                cell.Value = value;
+
+                Grid.Add(cell);
+            }
+
+            Console.WriteLine("value: " + value);
+            return value;
+        }
+    }
+
+    public class GridCell
+    {
+        public int XOffset { get; set; }
+        public int YOffset { get; set; }
+        public int Value { get; set; }
     }
 
     enum Direction

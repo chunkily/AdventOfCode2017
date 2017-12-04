47b9527f639e62741b3c32150370fe001710ce0a
Lee Chengkai <chengkai@tecedgesolutions.com>
2017-12-04 14:15:22 +0800

    Finish 4-1

---
+++ Day04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PartOne(new string[] {
+            //    "aa bb cc dd ee",
+            //    "aa bb cc dd aa",
+            //    "aa bb cc dd aaa"
+            //});
+            PartOne(File.ReadAllLines(args[0]));
+
+            Console.ReadLine();
+        }
+
+        static void PartOne(string[] lines)
+        {
+            var valid = lines.Count(IsPassPhraseValid);
+            Console.WriteLine(valid + " passphrases are valid");
+        }
+
+        static bool IsPassPhraseValid(string passphrase)
+        {
+            var words = passphrase.Split(" ");
+            return words.ToHashSet().Count() == words.Count();
+        }
+
+        static void PartTwo(string[] lines)
+        {
+
+        }
+    }
+}

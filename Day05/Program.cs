e84d14660bf7ded576dcfcb3f98564aa68535658
Lee Chengkai <chengkai@tecedgesolutions.com>
2017-12-05 16:48:52 +0800

    Finish 5-2

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -8,16 +8,22 @@ namespace Day05
     {
         static void Main(string[] args)
         {
-            // PartOne(new List<int>() {
-            //    0, 3, 0 ,1, -3
-            // });
             var input = File.ReadAllLines(args[0]);
             var list = new List<int>();
             foreach (var line in input)
             {
                 list.Add(Int32.Parse(line));
             }
-            PartOne(list);
+
+            var testList = new List<int>()
+            {
+                0, 3, 0 ,1, -3
+            };
+            // PartOne(testList);
+            // PartOne(list);
+
+            // PartTwo(testList);
+            PartTwo(list);
 
             Console.ReadLine();
         }
@@ -32,6 +38,30 @@ namespace Day05
 
                 int jump = instructions[currentIndex];
                 instructions[currentIndex]++;
+
+                currentIndex += jump;
+            }
+
+            Console.WriteLine(steps + " steps taken");
+        }
+
+        static void PartTwo(List<int> instructions)
+        {
+            var steps = 0;
+            int currentIndex = 0;
+            while (currentIndex >= 0 && currentIndex < instructions.Count)
+            {
+                steps++;
+                int jump = instructions[currentIndex];
+
+                if (jump >= 3)
+                {
+                    instructions[currentIndex]--;
+                }
+                else
+                {
+                    instructions[currentIndex]++;
+                }
 
                 currentIndex += jump;
             }

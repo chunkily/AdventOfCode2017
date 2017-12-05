5c0ca8bf33489d00e4598322b35b43d4235e3708
Lee Chengkai <chengkai@tecedgesolutions.com>
2017-12-05 16:42:51 +0800

    Finish 5-1

---
+++ Day05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // PartOne(new List<int>() {
+            //    0, 3, 0 ,1, -3
+            // });
+            var input = File.ReadAllLines(args[0]);
+            var list = new List<int>();
+            foreach (var line in input)
+            {
+                list.Add(Int32.Parse(line));
+            }
+            PartOne(list);
+
+            Console.ReadLine();
+        }
+
+        static void PartOne(List<int> instructions)
+        {
+            var steps = 0;
+            int currentIndex = 0;
+            while (currentIndex >= 0 && currentIndex < instructions.Count)
+            {
+                steps++;
+
+                int jump = instructions[currentIndex];
+                instructions[currentIndex]++;
+
+                currentIndex += jump;
+            }
+
+            Console.WriteLine(steps + " steps taken");
+        }
+    }
+}

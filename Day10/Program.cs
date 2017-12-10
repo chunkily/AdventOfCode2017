eb9558a397466c3ecfe9cb2cf0fdf80846523d91
Lee Chengkai <leechengkai@gmail.com>
2017-12-10 16:47:23 +0800

    Finish 10-2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Day10
 {
@@ -10,23 +11,56 @@ namespace Day10
     {
         static void Main(string[] args)
         {
-            // PartOne(5, new int[] { 3, 4, 1, 5 });
+            //var testArray = new CircularArray(5);
+            //PartOne(testArray, new int[] { 3, 4, 1, 5 });
+            //Console.WriteLine("first two values multiplied: " + testArray.MultiplyFirstTwo());
 
-            string[] inputText = File.ReadAllText(args[0]).Split(',');
-            int[] input = inputText.Select(i => { return Int32.Parse(i); }).ToArray();
-            PartOne(256, input);
+            string inputText = File.ReadAllText(args[0]);
+            //int[] input = inputText.Split(',').Select(i => { return Int32.Parse(i); }).ToArray();
+            //var circularArray = new CircularArray(256);
+            //PartOne(circularArray, input);
+            //Console.WriteLine("first two values multiplied: " + circularArray.MultiplyFirstTwo());
+
+            //Console.WriteLine(PartTwo(""));
+            //Console.WriteLine(PartTwo("AoC 2017"));
+            //Console.WriteLine(PartTwo("1,2,3"));
+            //Console.WriteLine(PartTwo("1,2,4"));
+            Console.WriteLine(PartTwo(inputText));
 
             Console.ReadLine();
         }
 
-        static void PartOne(int size, int[] lengths)
+        static void PartOne(CircularArray circularArray, int[] lengths)
         {
-            var circularArr = new CircularArray(size);
             foreach(var l in lengths)
             {
-                circularArr.Transform(l);
+                circularArray.Transform(l);
             }
-            Console.WriteLine("first two values multiplied: " + circularArr.MultiplyFirstTwo());
+        }
+
+        static string PartTwo(string input)
+        {
+            int[] lengths = new int[input.Length + 5];
+            for (int i = 0; i < input.Length; i++)
+            {
+                lengths[i] = input[i];
+            }
+            int[] suffix = new int[] { 17, 31, 73, 47, 23 };
+            suffix.CopyTo(lengths, input.Length);
+
+            var circularArray = new CircularArray(256);
+
+            for (int i = 0; i < 64; i++)
+            {
+                PartOne(circularArray, lengths);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach(var i in circularArray.GetDenseHash())
+            {
+                sb.AppendFormat("{0:x2}", i);
+            }
+            return sb.ToString();
         }
     }
 
@@ -70,6 +104,23 @@ namespace Day10
         public int MultiplyFirstTwo()
         {
             return arr[0] * arr[1];
+        }
+
+        public int[] GetDenseHash()
+        {
+            int[] denseHash = new int[16];
+
+            for (int i = 0; i < denseHash.Length; i++)
+            {
+                int first = i * 16;
+                var last = first + 16;
+                for (int j = first; j < last; j++)
+                {
+                    denseHash[i] = denseHash[i] ^ arr[j];
+                }
+            }
+
+            return denseHash;
         }
     }
 }
